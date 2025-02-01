4d498c24d9750d528f11ef286b069443dfaaea92
CortezS1222 <poopburbur@gmail.com>
2025-02-01 18:56:35 +0000

    added new page

--- hsccinfo/app.js
+++ hsccinfo/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bdpahistoryRouter = require('./routes/bdpahistory');
+
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/bdpahistory', bdpahistoryRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
